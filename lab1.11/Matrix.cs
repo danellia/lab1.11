d2f810d86e96aafbfe85caa7b6418b79433ee661
daria <deshkadary2@gmail.com>
2022-09-14 11:59:49 +0700

    Add project files.

---
+++ lab1.11/Matrix.cs
@@ -0,0 +1,68 @@
+﻿namespace lab1._11
+{
+    class Matrix
+    {
+        public static int size = 5;
+        int[,] matrix = new int[size, size];
+
+        public int this[int x, int y]
+        {
+            get
+            {
+                return matrix[x, y];
+            }
+            set
+            {
+                matrix[x, y] = value;
+            }
+        }
+
+        public int findMaxLeft()
+        {
+            int maxLeft = matrix[0, 0];
+            for (int i = 0; i < size - 1; ++i)
+            {
+                for (int j = 1; j < size - i - 1; ++j)
+                {
+                    if (matrix[i, j] >= maxLeft) maxLeft = matrix[i, j];
+                }
+            }
+            return maxLeft;
+        }
+
+        public int findMaxRight()
+        {
+            int maxRight = matrix[1, size - 1];
+            for (int i = 1; i < size; ++i)
+            {
+                for (int j = size - i; j < size; ++j)
+                {
+                    if (matrix[i, j] >= maxRight) maxRight = matrix[i, j];
+                }
+            }
+            return maxRight;
+        }
+
+        public override string ToString()
+        {
+            string result = "\n";
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    if (this[i, j] == 0)
+                    {
+                        result += "0      ";
+                    }
+                    else
+                    {
+                        result += String.Format("{0,-8}", this[i, j]);
+                    }
+                }
+                result += "\n";
+            }
+            result += "\n";
+            return result;
+        }
+    }
+}
---
+++ lab1.11/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Вариант 5
+Дана целочисленная квадратная матрица порядка 5. Найдите
+максимальный элемент среди элементов, лежащих левее вспомогательной
+диагонали, и максимальный элемент среди элементов, лежащих правее
+вспомогательной диагонали, поменяйте их местами*/
+namespace lab1._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix m = new Matrix();
+            for (int i = 1; i <= Matrix.size; ++i)
+            {
+                for (int j = 1; j <= Matrix.size; ++j)
+                {
+                    object? current = null;
+                    Console.WriteLine("Enter Matrix element[" + i + ", " + j + "]: ");
+                    while (current == null)
+                    {
+                        try
+                        {
+                            current = Convert.ToInt32(Console.ReadLine());
+                            m[i - 1, j - 1] = (int)current;
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Try again!");
+                            current = null;
+                        }
+                    }
+
+                }
+            }
+            Console.WriteLine(m.ToString());
+            Console.WriteLine("Max element from left: " + m.findMaxLeft());
+            Console.WriteLine("Max element from right: " + m.findMaxRight());
+        }
+    }
+}
